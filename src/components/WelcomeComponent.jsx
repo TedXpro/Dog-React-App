a5a70c26fb2ba4efc653252291d1c33da68acb20
TedXpro <johannes.woldeyes@gmail.com>
2023-08-24 17:02:49 +0300

    Add welcome and ListDogs components

---
+++ src/components/WelcomeComponent.jsx
@@ -0,0 +1,13 @@
+export default function WelcomeComponent() {
+    
+    return (
+        <div className="Welcome">
+            <h1>Welcome</h1>
+
+            <div>
+                Dog List - <Link to = "/dogs">Go here</Link>
+            </div>
+            
+        </div>
+    )
+}
